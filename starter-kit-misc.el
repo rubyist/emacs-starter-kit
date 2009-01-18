c1e847306a08cf18b006ddbda71626e5629d03cc
Scott Barron <scott@elitists.net>
2009-01-18 11:46:01 -0500

    -ido-use-filename-at-point fails in a stupid way if you're on a // comment

--- starter-kit-misc.el
+++ starter-kit-misc.el
@@ -61,7 +61,6 @@
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
         ido-create-new-buffer 'always
-        ido-use-filename-at-point t
         ido-max-prospects 10))
 
 (set-default 'indent-tabs-mode nil)
